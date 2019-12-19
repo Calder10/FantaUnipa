5d433f31cd2c9422bf8c445c483422aa373bfc99
Salvatore Calderaro <46534450+Calder10@users.noreply.github.com>
2019-12-19 17:29:50 +0100

    Creazione Fantallenatore tramite Builder

---
+++ FantallenatoreBuilder.java
@@ -0,0 +1,14 @@
+
+public interface FantallenatoreBuilder {
+	public void addNome();
+
+	public void addCognome();
+
+	public void addUsername();
+
+	public void addPassword();
+
+	public void addFantaCrediti();
+
+	public Fantallenatore getFantallenatore();
+}
