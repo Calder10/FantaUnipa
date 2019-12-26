138dcb2a1b3fd8f8b4e327af4433e65fb0bb1591
Calder10 <46534450+Calder10@users.noreply.github.com>
2019-12-26 15:56:29 +0100

    getter and setter Giocatore

--- src/Giocatore.java
+++ src/Giocatore.java
@@ -2,5 +2,19 @@
 public class Giocatore {
 	protected String nome;
 	protected String cognome;
+	public String getNome() {
+		return nome;
+	}
+	public void setNome(String nome) {
+		this.nome = nome;
+	}
+	public String getCognome() {
+		return cognome;
+	}
+	public void setCognome(String cognome) {
+		this.cognome = cognome;
+	}
+	
+	
 	
 }
