dba1d934a7d85898b74da6e117e9d5596de7471e
Salvatore Calderaro <salvatorecalderaro94@libero.it>
2020-01-13 18:48:55 +0100

    commit

--- src/Classifica.java
+++ src/Classifica.java
@@ -8,26 +8,26 @@ import java.io.ObjectOutputStream;
 import java.io.Serializable;
 import java.util.ArrayList;
 
-public class Classifica implements Serializable{
+public class Classifica implements Serializable {
 
 	/**
 	 * 
 	 */
 	private static final long serialVersionUID = -3870458223506593474L;
 	private ArrayList<ArrayList<Object>> classifica;
-	
+
 	public ArrayList<ArrayList<Object>> getClassifica() {
 		return classifica;
 	}
 
-
 	public void setClassifica(ArrayList<ArrayList<Object>> classifica) {
 		this.classifica = classifica;
 	}
+
 	public Classifica(ArrayList<Squadra> squadre) {
 		classifica = new ArrayList<>();
-		int i=1;
-		for(Squadra s: squadre) {
+		int i = 1;
+		for (Squadra s : squadre) {
 			ArrayList<Object> aus = new ArrayList<Object>();
 			aus.add(i);
 			aus.add(s.getPathLogo());
@@ -39,74 +39,110 @@ public class Classifica implements Serializable{
 			i++;
 			this.getClassifica().add(aus);
 		}
-		
+
 	}
-	
+
 	public static void updateClassifica(Giornata g) throws IOException, ClassNotFoundException {
-		File f = new File ("src/torneo.dat");
+		File f = new File("src/torneo.dat");
 		FileInputStream fis = new FileInputStream(f);
 		ObjectInputStream ois = new ObjectInputStream(fis);
 		Torneo t = (Torneo) ois.readObject();
 		ois.close();
 		fis.close();
-		 ArrayList<Partita> partite = g.getPartite().getPartite();
-		 ArrayList<Object> aus= new ArrayList<Object>();
-		 String [] fields;
-		 ArrayList<ArrayList<Object>> c= t.getClassifica().getClassifica();
-		 for(Partita p : partite) {
-			 fields=p.getRisultato().split("-");
-			 int casa = Integer.parseInt(fields[0]);
-			 int trasferta = Integer.parseInt(fields[1]);
-			 for (ArrayList<Object> o : c) {
-				 for(ArrayList<Object> o1 :c) {
-					 if(((String) o.get(2)).equalsIgnoreCase(p.getSquadraCasa().getNomeSquadra())&& ((String) o1.get(2)).equalsIgnoreCase(p.getSquadraTrasferta().getNomeSquadra())) {
-						 if(casa==trasferta) {
-							 
-							 o.set(7,((Integer) o.get(6)+1));
-							 o.set(4, fields[0]);
-							 o.set(5,fields[1]);
-							 o.set(3, ((Integer)o.get(4)- (Integer)o.get(5)));
-							 
-							 o1.set(7,(Integer)o1.get(6)+1);
-							 o1.set(4, fields[1]);
-							 o1.set(5,fields[0]);
-							 o1.set(3, ((Integer) o1.get(4)- (Integer)o1.get(5)));
-						 }
-						if(casa>trasferta) {
-							o.set(7,(Integer) o.get(6)+3);
-							 o.set(4, fields[0]);
-							 o.set(5,fields[1]);
-							 o.set(3, ((Integer) o.get(4)- (Integer) o.get(5)));
-							 
-							 o1.set(7,(Integer)o.get(6)+0);
-							 o1.set(4, fields[1]);
-							 o1.set(5,fields[0]);
-							 o1.set(3, ((Integer) o1.get(4)- (Integer)o1.get(5)));
-						}
-						
-						if(casa<trasferta) {
-							o.set(7,(Integer) o.get(6)+0);
-							 o.set(4, fields[0]);
-							 o.set(5,fields[1]);
-							 o.set(3, ((Integer) o.get(4)- (Integer) o.get(5)));
-							 
-							 o1.set(7,(Integer)o.get(6)+3);
-							 o1.set(4, fields[1]);
-							 o1.set(5,fields[0]);
-							 o1.set(3, ((Integer)o1.get(4)- (Integer)o1.get(5)));
-						}
-					 }
-				 }
-			 }
-		 }
-		 // metodo sorting;
-		 t.getClassifica().setClassifica(c);
-		 File f1 = new File("torneo1.dat");
-		 FileOutputStream fos = new FileOutputStream(f1);
-		 ObjectOutputStream oos = new ObjectOutputStream(fos);
-		 oos.writeObject(t);
-		 oos.close();
-		 fos.close();
-		 f1.renameTo(f);
+		ArrayList<Partita> partite = g.getPartite().getPartite();
+		String[] fields;
+		ArrayList<ArrayList<Object>> c = t.getClassifica().getClassifica();
+		ArrayList<Object> a = new ArrayList<Object>();
+		ArrayList<Object> b = new ArrayList<Object>();
+		for (Partita p : partite) {
+			fields = p.getRisultato().split("-");
+			int casa = Integer.parseInt(fields[0].trim());
+			int trasferta = Integer.parseInt(fields[1].trim());
+			int x = 0;
+			int y = 0;
+			for (ArrayList<Object> o : c) {
+				if (o.get(2).toString().equalsIgnoreCase(p.getSquadraCasa().getNomeSquadra())) {
+					if (casa == trasferta) {
+						int i = ((Integer) o.get(6)).intValue();
+						o.set(6, i + 1);
+						o.set(4, casa);
+						o.set(5, trasferta);
+						int gf = ((Integer) o.get(4)).intValue();
+						int gs = ((Integer) o.get(5)).intValue();
+						o.set(3, gf-gs);
+					}
+
+					if (casa > trasferta) {
+						int i = ((Integer) o.get(6)).intValue();
+						o.set(6, i+3);
+						o.set(4, casa);
+						o.set(5, trasferta);
+						int gf = ((Integer) o.get(4)).intValue();
+						int gs = ((Integer) o.get(5)).intValue();
+						o.set(3, gf-gs);
+					}
+
+					if (casa < trasferta) {
+						int i = ((Integer) o.get(6)).intValue();
+						o.set(6, i + 0);
+						o.set(4, casa);
+						o.set(5, trasferta);
+						int gf = ((Integer) o.get(4)).intValue();
+						int gs = ((Integer) o.get(5)).intValue();
+						o.set(3, gf-gs);
+					}
+
+				}
+
+			}
+
+		}
+		for (Partita p : partite) {
+			fields = p.getRisultato().split("-");
+			int casa = Integer.parseInt(fields[0].trim());
+			int trasferta = Integer.parseInt(fields[1].trim());
+			for (ArrayList<Object> o1 : c) {
+				if (o1.get(2).toString().equalsIgnoreCase(p.getSquadraTrasferta().getNomeSquadra())) {
+					if (casa == trasferta) {
+						int i = ((Integer) o1.get(6)).intValue();
+						o1.set(6, Integer.valueOf((String) o1.get(6)) + 1);
+						o1.set(4, trasferta);
+						o1.set(5, casa);
+						int gf = ((Integer) o1.get(4)).intValue();
+						int gs = ((Integer) o1.get(5)).intValue();
+						o1.set(3, gf-gs);
+					}
+
+					if (casa > trasferta) {
+						int i = ((Integer) o1.get(6)).intValue();
+						o1.set(6, i + 0);
+						o1.set(4, trasferta);
+						o1.set(5, casa);
+						int gf = ((Integer) o1.get(4)).intValue();
+						int gs = ((Integer) o1.get(5)).intValue();
+						o1.set(3, gf-gs);
+					}
+					
+					if (casa < trasferta) {
+						int i = ((Integer) o1.get(6)).intValue();
+						o1.set(6, i + 0);
+						o1.set(4, trasferta);
+						o1.set(5, casa);
+						int gf = ((Integer) o1.get(4)).intValue();
+						int gs = ((Integer) o1.get(5)).intValue();
+						o1.set(3, gf-gs);
+					}
+				}
+			}
+		}
+		// metodo sorting;
+		t.getClassifica().setClassifica(c);
+		File f1 = new File("torneo1.dat");
+		FileOutputStream fos = new FileOutputStream(f1);
+		ObjectOutputStream oos = new ObjectOutputStream(fos);
+		oos.writeObject(t);
+		oos.close();
+		fos.close();
+		f1.renameTo(f);
 	}
 }
